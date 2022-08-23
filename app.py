447d5239e3228357bb7516178179efa6bf801707
mBajus <103207739+mBajus@users.noreply.github.com>
2022-08-23 11:37:11 +0200

    herokutest

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask, jsonify
+from unittest import expectedFailure
+from urllib import request
+from flask import Flask, jsonify, request
+import sqlite3
 
 app = Flask(__name__)
+
+def db_connection():
+    conn = None
+    try:
+        conn = sqlite3.connect("lottodb.sqlite")
+    except sqlite3.error as e:
+        print(e)
+    return conn
 
 
 @app.route('/')
 def index():
+    return "Lotto API"
 
 
-app.run()
+if __name__ == "__main__":
+    app.run()
