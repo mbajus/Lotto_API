a547db8fbf42bb8c8bc64dc4a2a13bcd9197a7c8
mBajus <103207739+mBajus@users.noreply.github.com>
2022-08-24 11:12:31 +0200

    heroku fix

--- flask_api/__init__.py
+++ flask_api/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 from .commands import create_tables
 from .extensions import db
-
+from .routes.main import main
 
 def create_app(config_file='settings.py'):
     app = Flask(__name__)
@@ -10,5 +10,7 @@ def create_app(config_file='settings.py'):
     app.config.from_pyfile(config_file)
     db.init_app(app)
     app.register_blueprint(main)
+
+    app.cli.add_command(create_tables)
 
     return app
