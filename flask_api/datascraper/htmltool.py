9429e2b4e82f975fa23c6094762b978577a62c0a
mBajus <103207739+mBajus@users.noreply.github.com>
2022-08-31 15:03:28 +0200

    fix missing get at selenium

--- flask_api/datascraper/htmltool.py
+++ flask_api/datascraper/htmltool.py
@@ -8,6 +8,7 @@ def gethtml(url):
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+    driver.get(url)
     page = driver.page_source
     driver.close()
     return page
