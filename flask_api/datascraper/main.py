65c586dba8648fe75ef9ad487c4cc9f92a4ee6dd
mBajus <103207739+mBajus@users.noreply.github.com>
2022-09-07 11:41:57 +0200

    fix typo

--- flask_api/datascraper/main.py
+++ flask_api/datascraper/main.py
@@ -5,7 +5,7 @@ import time, os
 def update():
     os.environ['TZ'] = 'Europe/Warsaw'
     date = time.strftime('%Y%m%d')
-    records = Lotto.query.with_entities(Lotto.id, Lotto.date)
+    records = Lotto.query.with_entities(Lotto.id, Lotto.date).all()
     print(records)
     
 def lastupdate():
