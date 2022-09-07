2d735ffe502f16311772e9fb322f153ce68bdb60
mBajus <103207739+mBajus@users.noreply.github.com>
2022-09-07 16:58:12 +0200

    autoscraping checks

--- flask_api/datascraper/main.py
+++ flask_api/datascraper/main.py
@@ -1,14 +1,21 @@
 from ..models import Lotto
 from ..extensions import db
+from .scraper import scrap_to_db
 import time, os
 
+
 def update():
-    os.environ['TZ'] = 'Europe/Warsaw'
-    date = time.strftime('%Y%m%d')
-    records = Lotto.query.with_entities(Lotto.id, Lotto.date).all()
-    print(records)
+    lastupdate()  
+    ids = []
+    for record in Lotto.query.with_entities(Lotto.id).all(): # getting list of lottery IDs in DB
+        ids.append(record[0])
+    print(ids)
+    ids.sort(reverse=True)
+    print(ids)
+    
     
 def lastupdate():
-    pass
+    url = "https://www.lotto.pl/lotto/wyniki-i-wygrane"
+    scrap_to_db(url)
 
 
