3540e8a79491a831127e3037cad37660276a4fc5
mBajus <103207739+mBajus@users.noreply.github.com>
2022-09-08 14:29:20 +0200

    setting clock

--- clock.py
+++ clock.py
@@ -5,10 +5,17 @@ sched = BlockingScheduler(timezone='Europe/Warsaw')
 
 # The clock sends to API a request, which start an update.
 # checks if up to date
-@sched.scheduled_job('interval', minutes=5, id='interval_check')
+@sched.scheduled_job('interval', minutes=120, id='interval_check')
 def timed_job():
     print('Sending request for update - every 2h.')
-    update()
+    count = 0
+    last = update()
+    while last != 0 or count != last:
+        count = last
+        print(f"Still missing {last} records of Lottery.")
+        last = update()
+    print("None records missing, the datebase is up to date.")
+
 
 # gets the newest scores
 @sched.scheduled_job('cron', day_of_week='1,3,5', hour=22, minute=10, id='lottery_check')
