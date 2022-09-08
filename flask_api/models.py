0967343b3dec4893aeab88edf8731d8fc80866e9
mBajus <103207739+mBajus@users.noreply.github.com>
2022-09-08 13:30:59 +0200

    fix model Lotto

--- flask_api/models.py
+++ flask_api/models.py
@@ -17,4 +17,6 @@ class Lotto(Base):
     def __init__(self, id, nums, numsp, date, time):
         id.self = id
         nums.self = nums
+        numsp.self = numsp
         date.self = date
+        time.self = time
