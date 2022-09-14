16a941babac3164445e58d868ec116b34f9e4315
mBajus <103207739+mBajus@users.noreply.github.com>
2022-09-14 16:18:30 +0200

    typo fix

--- flask_api/settings.py
+++ flask_api/settings.py
@@ -1,5 +1,5 @@
 import os 
 
-SQLALCHEMY_DATABASE_URI = os.environ.get('AWS_DATABASE', 'postgresql://dbadmin:DOuJCmcliCYR0MeVxEfZ@lottery-database.c4oeaaldfp8m.us-east-1.rds.amazonaws.com:5432/lotto')
+SQLALCHEMY_DATABASE_URI = os.environ.get('AWS_DATABASE')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 JSON_SORT_KEYS = False
