1f65ae7e77c0172cb25db97bb5b5a1d32663da86
mBajus <103207739+mBajus@users.noreply.github.com>
2022-09-14 16:26:07 +0200

    fix import typo

--- flask_api/models.py
+++ flask_api/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import MetaData, Column, Integer, String
+from sqlalchemy import MetaData, Column, Integer, String, ForeignKey 
 from sqlalchemy.ext.declarative import declarative_base
 
 metadata = MetaData()
