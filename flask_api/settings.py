36e3cfc907e010c343e71c410581bab15cba899e
mBajus <103207739+mBajus@users.noreply.github.com>
2022-09-14 11:02:55 +0200

    changing db

--- flask_api/settings.py
+++ flask_api/settings.py
@@ -1,5 +1,5 @@
 import os 
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://ukjqjsatnawsnr:f717d4c1a574882a1589dfb9f070a7bb5ff568277dda1464a4a18fe0216c6ae6@ec2-34-241-90-235.eu-west-1.compute.amazonaws.com:5432/delug4lrrcpmt8'
+SQLALCHEMY_DATABASE_URI = os.environ.get('AWS_DATABASE')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 JSON_SORT_KEYS = False
